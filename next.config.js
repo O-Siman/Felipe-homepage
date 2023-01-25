d5ffd4c2c1c9ec5caf3c01191b49142fce24e8f4
felos9001 <90056233+Felos9001@users.noreply.github.com>
2023-01-24 19:09:43 -0500

    new changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true
-}
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig   = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
+
+export default   nextConfig;
